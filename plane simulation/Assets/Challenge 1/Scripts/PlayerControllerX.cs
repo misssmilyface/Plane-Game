3873b00fef557cc15289424f950bda3d22f3e842
misssmilyface <niafreya123@gmail.com>
2023-11-21 03:38:00 +0800

    Plane Game

---
+++ plane simulation/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 12.0f;
+    private float rotationSpeed = 50.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * horizontalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+        /*
+        up : left to right (rotation ex. car)
+        down: right to left (rotation ex.car)
+        left: up to "up arrow", down to "down arrow" (ex. plane)
+        right: up to "down arrow", down to "up arrow" (ex. plane)
+        */
+    }
+}
